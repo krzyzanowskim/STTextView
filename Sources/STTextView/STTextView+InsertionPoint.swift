da1365428b503944abd49ffa6b8ee11e8831b6ad
Marcin Krzyzanowski <marcin.krzyzanowski@gmail.com>
2022-03-31 00:41:53 +0200

    Workaround height for insertion point at empty document

--- Sources/STTextView/STTextView+InsertionPoint.swift
+++ Sources/STTextView/STTextView+InsertionPoint.swift
@@ -16,12 +16,23 @@ extension STTextView {
         if shouldDrawInsertionPoint {
             for textRange in textLayoutManager.textSelections.flatMap(\.textRanges) {
                 textLayoutManager.enumerateTextSegments(in: textRange, type: .selection, options: .rangeNotRequired) { ( _, textSegmentFrame, baselinePosition, textContainer) in
-                    let highlightFrame = textSegmentFrame.intersection(frame)
-                    guard !highlightFrame.isNull else {
+                    var selectionFrame = textSegmentFrame.intersection(frame)
+                    guard !selectionFrame.isNull else {
                         return true
                     }
 
-                    let insertionView = (Self.insertionPointClass as STInsertionPoint.Type).init(frame: highlightFrame)
+                    // because `textLayoutManager.enumerateTextLayoutFragments(from: nil, options: [.ensuresExtraLineFragment, .ensuresLayout, .estimatesSize])`
+                    // returns unexpected value for extra line fragment height (return 14) that is not correct in the context,
+                    // therefore for empty override height with value manually calculated from font + paragraph style
+                    if textRange == documentRange {
+                        if let font = typingAttributes[.font] as? NSFont {
+                            let paragraphStyle = typingAttributes[.paragraphStyle] as? NSParagraphStyle ?? NSParagraphStyle.default
+                            let lineHeight = NSLayoutManager().defaultLineHeight(for: font) * paragraphStyle.lineHeightMultiple
+                            selectionFrame = NSRect(origin: selectionFrame.origin, size: CGSize(width: selectionFrame.width, height: lineHeight))
+                        }
+                    }
+
+                    let insertionView = (Self.insertionPointClass as STInsertionPoint.Type).init(frame: selectionFrame)
                     assert(insertionView.isFlipped)
                     insertionView.insertionPointColor = insertionPointColor
 
