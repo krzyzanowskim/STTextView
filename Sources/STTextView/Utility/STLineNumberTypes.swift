1addd96d18fbfdb7b63ed3178d9e6eb08a3a927f
Elias Wahl <Eliasruben2204@gmail.com>
2023-01-23 22:48:05 +0100

    Changed Double values in rulerWidth-struct to CGFloat

--- Sources/STTextView/Utility/STLineNumberTypes.swift
+++ Sources/STTextView/Utility/STLineNumberTypes.swift
@@ -1,10 +1,11 @@
 //  Created by Elias Wahl on 14.01.23.
+import Foundation
 
 public struct STRulerInsets: Equatable {
-    public let leading: Double
-    public let trailing: Double
+    public let leading: CGFloat
+    public let trailing: CGFloat
     
-    public init(leading: Double, trailing: Double) {
+    public init(leading: CGFloat, trailing: CGFloat) {
         self.leading = leading
         self.trailing = trailing
     }
