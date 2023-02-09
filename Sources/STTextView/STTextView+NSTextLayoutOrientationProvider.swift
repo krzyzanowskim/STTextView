edc9dda8dd61f62254869c3c64d11ab924a1b24d
Marcin Krzyzanowski <marcin.krzyzanowski@gmail.com>
2023-02-09 13:52:01 +0100

    NSTextLayoutOrientationProvider

---
+++ Sources/STTextView/STTextView+NSTextLayoutOrientationProvider.swift
@@ -0,0 +1,17 @@
+//  Created by Marcin Krzyzanowski
+//  https://github.com/krzyzanowskim/STTextView/blob/main/LICENSE.md
+
+import Cocoa
+
+extension STTextView: NSTextLayoutOrientationProvider {
+    public var layoutOrientation: NSLayoutManager.TextLayoutOrientation {
+        switch textLayoutManager.textLayoutOrientation(at: textLayoutManager.documentRange.location) {
+        case .horizontal:
+            return .horizontal
+        case .vertical:
+            return .vertical
+        @unknown default:
+            return textContainer.layoutOrientation
+        }
+    }
+}
