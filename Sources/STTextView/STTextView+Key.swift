e05b7901371cd10244247b051f4a147150de7ff0
Marcin Krzyzanowski <marcin.krzyzanowski@gmail.com>
2023-07-12 21:56:48 +0300

    Handle custom keyboard shortcut in performKeyEquivalent

--- Sources/STTextView/STTextView+Key.swift
+++ Sources/STTextView/STTextView+Key.swift
@@ -18,14 +18,27 @@ extension STTextView {
 
         NSCursor.setHiddenUntilMouseMoves(true)
 
-        // ^Space -> complete:
-        if event.modifierFlags.contains(.control) && event.charactersIgnoringModifiers == " " {
-            doCommand(by: #selector(NSStandardKeyBindingResponding.complete(_:)))
-            return
-        }
-
         if inputContext?.handleEvent(event) == false {
             interpretKeyEvents([event])
         }
+    }
+
+    open override func performKeyEquivalent(with event: NSEvent) -> Bool {
+        guard isEditable else {
+            return super.performKeyEquivalent(with: event)
+        }
+
+        processingKeyEvent = true
+        defer {
+            processingKeyEvent = false
+        }
+
+        // ^Space -> complete:
+        if event.modifierFlags.contains(.control) && event.charactersIgnoringModifiers == " " {
+            doCommand(by: #selector(NSStandardKeyBindingResponding.complete(_:)))
+            return true
+        }
+
+        return super.performKeyEquivalent(with: event)
     }
 }
