2c73ef2010e1f41cfc89c919199916c7ab203382
Marcin Krzyzanowski <marcin.krzyzanowski@gmail.com>
2023-07-16 22:57:07 +0200

    Calculate min/max X for viewport bounds

--- Sources/STTextView/STTextView+NSTextViewportLayoutControllerDelegate.swift
+++ Sources/STTextView/STTextView+NSTextViewportLayoutControllerDelegate.swift
@@ -9,19 +9,25 @@ extension STTextView: NSTextViewportLayoutControllerDelegate {
         let overdrawRect = preparedContentRect
         let minY: CGFloat
         let maxY: CGFloat
+        let minX: CGFloat
+        let maxX: CGFloat
 
         if !overdrawRect.isEmpty, overdrawRect.intersects(visibleRect) {
             // Use preparedContentRect for vertical overdraw and ensure visibleRect is included at the minimum,
             // the width is always bounds width for proper line wrapping.
+            minX = min(overdrawRect.minX, max(visibleRect.minX, bounds.minX))
             minY = min(overdrawRect.minY, max(visibleRect.minY, bounds.minY))
+            maxX = max(overdrawRect.maxX, visibleRect.maxX)
             maxY = max(overdrawRect.maxY, visibleRect.maxY)
         } else {
             // We use visible rect directly if preparedContentRect does not intersect.
             // This can happen if overdraw has not caught up with scrolling yet, such as before the first layout.
+            minX = visibleRect.minX
             minY = visibleRect.minY
+            maxX = visibleRect.maxX
             maxY = visibleRect.maxY
         }
-        return CGRect(x: bounds.minX, y: minY, width: visibleRect.width, height: maxY - minY)
+        return CGRect(x: minX, y: minY, width: maxX, height: maxY - minY)
     }
 
     public func textViewportLayoutControllerWillLayout(_ textViewportLayoutController: NSTextViewportLayoutController) {
