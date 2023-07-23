c8956dca73bfa2224797fcfeb31c1e8956b90800
Marcin Krzyzanowski <marcin.krzyzanowski@gmail.com>
2023-07-23 19:16:17 +0200

    refine condition of empty document

--- Sources/STTextView/STTextView+InsertionPoint.swift
+++ Sources/STTextView/STTextView+InsertionPoint.swift
@@ -23,7 +23,7 @@ extension STTextView {
                 // because `textLayoutManager.enumerateTextLayoutFragments(from: nil, options: [.ensuresExtraLineFragment, .ensuresLayout, .estimatesSize])`
                 // returns unexpected value for extra line fragment height (return 14) that is not correct in the context,
                 // therefore for empty override height with value manually calculated from font + paragraph style
-                if textRange.isEmpty, textContentManager.documentRange.isEmpty {
+                if textRange == textContentManager.documentRange, textRange.isEmpty {
                     return CGRect(origin: selectionFrame.origin, size: CGSize(width: selectionFrame.width, height: typingLineHeight)).pixelAligned
                 }
 
