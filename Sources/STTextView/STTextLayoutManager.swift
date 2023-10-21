da6c7594b676c722478ed304e5fb4d4162968307
Marcin Krzyzanowski <marcin.krzyzanowski@gmail.com>
2023-10-21 19:02:41 +0200

    Add runtime check for bounds workaround

--- Sources/STTextView/STTextLayoutManager.swift
+++ Sources/STTextView/STTextLayoutManager.swift
@@ -5,6 +5,8 @@ import AppKit
 
 final class STTextLayoutManager: NSTextLayoutManager {
 
+    private static let needsBoundsWorkaround = testIfNeedsBoundsWorkaround()
+
     override var textSelections: [NSTextSelection] {
         didSet {
             let notification = Notification(name: STTextView.didChangeSelectionNotification, object: self, userInfo: nil)
@@ -14,10 +16,40 @@ final class STTextLayoutManager: NSTextLayoutManager {
 
     override var usageBoundsForTextContainer: CGRect {
         var rect = super.usageBoundsForTextContainer
-        // FB13290979: NSTextContainer.lineFragmentPadding does not affect end of the fragment usageBoundsForTextContainer rectangle
-        // https://gist.github.com/krzyzanowskim/7adc5ee66be68df2f76b9752476aadfb
-        rect.size.width += textContainer?.lineFragmentPadding ?? 0
+        if Self.needsBoundsWorkaround {
+            // FB13290979: NSTextContainer.lineFragmentPadding does not affect end of the fragment usageBoundsForTextContainer rectangle
+            // https://gist.github.com/krzyzanowskim/7adc5ee66be68df2f76b9752476aadfb
+            rect.size.width += textContainer?.lineFragmentPadding ?? 0
+        }
         return rect
     }
 
+}
+
+
+// FB13290979: NSTextContainer.lineFragmentPadding does not affect end of the fragment usageBoundsForTextContainer rectangle
+private func testIfNeedsBoundsWorkaround() -> Bool {
+    let textContentManager = NSTextContentStorage()
+    let textLayoutManager = NSTextLayoutManager()
+    textContentManager.addTextLayoutManager(textLayoutManager)
+    textContentManager.primaryTextLayoutManager = textLayoutManager
+
+    let textContainer = NSTextContainer()
+    textLayoutManager.textContainer = textContainer
+
+    textContentManager.attributedString = NSAttributedString(string: "01234567890123456789")
+
+    textContainer.lineFragmentPadding = 5
+    textLayoutManager.ensureLayout(for: textContentManager.documentRange)
+    let bounds1 = textLayoutManager.usageBoundsForTextContainer
+
+    textContainer.lineFragmentPadding = 0
+    textLayoutManager.ensureLayout(for: textContentManager.documentRange)
+    let bounds2 = textLayoutManager.usageBoundsForTextContainer
+
+    if bounds1.width == bounds2.width {
+        return true
+    }
+
+    return false
 }
