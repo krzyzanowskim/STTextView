ebc2f40b37b3456fa3f66f071c25f67103664926
Marcin Krzyzanowski <marcin.krzyzanowski@gmail.com>
2024-05-14 13:34:43 +0200

    add ifdef

--- Sources/STObjCLandShim/include/CGContextShim.h
+++ Sources/STObjCLandShim/include/CGContextShim.h
@@ -3,7 +3,9 @@
 
 #import <Cocoa/Cocoa.h>
 
+#ifdef USE_FONT_SMOOTHING_STYLE
 void STContextSetFontSmoothingStyle(CGContextRef context, int style);
 int STContextGetFontSmoothingStyle(CGContextRef context);
+#endif
 
 #endif /* CGContextShim_h */
