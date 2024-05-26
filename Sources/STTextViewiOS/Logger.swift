4545a1ee7f57d7c559eef1b8846f51c4136778fd
Marcin Krzyzanowski <marcin.krzyzanowski@gmail.com>
2024-05-26 23:51:31 +0200

    Add logger to ios module

---
+++ Sources/STTextViewiOS/Logger.swift
@@ -0,0 +1,7 @@
+//  Created by Marcin Krzyzanowski
+//  https://github.com/krzyzanowskim/STTextView/blob/main/LICENSE.md
+
+import Foundation
+import OSLog
+
+internal let logger = Logger(subsystem: "best.swift.sttextview", category: "STTextView")
