81fa9003cdc3bac206cc96664a73a3143243a637
Marcin Krzyzanowski <marcin.krzyzanowski@gmail.com>
2024-06-29 14:04:40 +0200

    cleanup content scale

--- Sources/STTextViewUIKit/STTextLayoutFragmentView.swift
+++ Sources/STTextViewUIKit/STTextLayoutFragmentView.swift
@@ -12,7 +12,31 @@ final class STTextLayoutFragmentView: UIView {
     init(layoutFragment: NSTextLayoutFragment, frame: CGRect) {
         self.layoutFragment = layoutFragment
         super.init(frame: frame)
+        layoutGlyphLayers()
+    }
 
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        super.traitCollectionDidChange(previousTraitCollection)
+        updateLayerContentScale()
+    }
+
+    override func didMoveToWindow() {
+        super.didMoveToWindow()
+        updateLayerContentScale()
+    }
+
+    private func updateLayerContentScale() {
+        let newScale = window?.screen.scale ?? UIScreen.main.scale
+        layer.contentsScale = newScale
+        layer.sublayers?.forEach { $0.contentsScale = newScale }
+    }
+
+    private func layoutGlyphLayers() {
         // layout each character on separate layer, so it can be animable
         for textLineFragment in layoutFragment.textLineFragments {
             let lineAttributedString = textLineFragment.attributedString.attributedSubstring(from: textLineFragment.characterRange)
@@ -26,7 +50,6 @@ final class STTextLayoutFragmentView: UIView {
                     let glyphPosition = glyphPositions[idx]
                     let boundingRect = boundingRects[idx]
                     let glyphLayer = GlyphLayer(font: run.font, color: run.attributes[.foregroundColor] as? UIColor ?? UIColor.label, glyph: glyph)
-                    glyphLayer.contentsScale = 2 // layer.contentsScale
 
                     glyphLayer.frame = CGRect(
                         x: textLineFragment.typographicBounds.origin.x + glyphPosition.x,
@@ -39,47 +62,9 @@ final class STTextLayoutFragmentView: UIView {
 
                     glyphLayer.setNeedsDisplay()
                     layer.addSublayer(glyphLayer)
-
-                    do {
-                        let animation1 = CABasicAnimation(keyPath: "transform.translation.x")
-                        animation1.fromValue = Double.random(in: -2...0)
-                        animation1.toValue = Double.random(in: 0...2)
-                        animation1.timingFunction = .init(name: .easeInEaseOut)
-
-                        let animation2 = CABasicAnimation(keyPath: "transform.translation.y")
-                        animation2.fromValue = Double.random(in: -2...0)
-                        animation2.toValue = Double.random(in: 0...1)
-                        animation2.timingFunction = .init(name: .linear)
-
-                        let animation3 = CABasicAnimation(keyPath: "transform.rotation.y")
-                        animation3.fromValue = Double.random(in: -(30 * .pi / 180)...0)
-                        animation3.toValue = Double.random(in: 0...(30 * .pi / 180))
-                        animation3.timingFunction = .init(name: .linear)
-
-                        let animation4 = CABasicAnimation(keyPath: "transform.rotation.z")
-                        animation4.fromValue = Double.random(in: -(15 * .pi / 180)...0)
-                        animation4.toValue = Double.random(in: 0...(15 * .pi / 180))
-                        animation4.timingFunction = .init(name: .linear)
-
-                        let groupAnimation = CAAnimationGroup()
-                        groupAnimation.duration = Double.random(in: 1..<2)
-                        groupAnimation.animations = [animation1, animation2, animation3, animation4]
-                        groupAnimation.isRemovedOnCompletion = false
-                        groupAnimation.autoreverses = true
-                        groupAnimation.repeatCount = .greatestFiniteMagnitude
-
-                        glyphLayer.add(groupAnimation, forKey: "itsmine")
-                    }
                 }
             }
         }
-
-        setNeedsDisplay()
-    }
-
-    @available(*, unavailable)
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
     }
 
     override func layoutSubviews() {
