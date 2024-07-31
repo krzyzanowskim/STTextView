baf4a76ac3c80d6d89b56a19aca8945103860e5c
Marcin Krzyzanowski <marcin.krzyzanowski@gmail.com>
2024-07-31 22:26:22 +0200

    fix boundary recognition

--- Sources/STTextViewUIKit/STTextInputTokenizer.swift
+++ Sources/STTextViewUIKit/STTextInputTokenizer.swift
@@ -59,7 +59,7 @@ final class STTextInputTokenizer: NSObject, UITextInputTokenizer {
             direction: direction.textSelectionNavigationDirection,
             destination: granularity.textSelectionDestination,
             extending: false,
-            confined: true
+            confined: false
         )
 
         return destination?.textRanges.first?.location.uiTextPosition
