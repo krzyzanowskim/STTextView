033da79ae11aedc9898cfdd886cf4659481e992c
Marcin Krzyzanowski <marcin.krzyzanowski@gmail.com>
2024-08-22 13:29:04 +0200

    Update demo apps

--- TextEdit/iOS/ViewController.swift
+++ TextEdit/iOS/ViewController.swift
@@ -24,7 +24,7 @@ class ViewController: UIViewController {
         textView.font = UIFont.monospacedSystemFont(ofSize: 14, weight: .regular)
         textView.text = try! String(contentsOf: Bundle.main.url(forResource: "content", withExtension: "txt")!)
         textView.showsLineNumbers = true
-        textView.showsInvisibleCharacters = true
+        textView.showsInvisibleCharacters = false
         textView.gutterView?.drawSeparator = true
         view.addSubview(textView)
         self.textView = textView
