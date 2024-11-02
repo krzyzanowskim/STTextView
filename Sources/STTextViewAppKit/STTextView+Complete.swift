984e18c6ea7b3f958cc30eaf4e75009702c10d97
Marcin Krzyzanowski <marcin.krzyzanowski@gmail.com>
2024-11-02 22:36:32 +0100

    Add @MainActor annotations to improve thread safety in completion handling
    This commit applies the @MainActor attribute to methods associated with
    autocomplete functionality in STTextView. This ensures that operations
    that involve UI updates occur on the main thread, preventing potential
    threading issues. Additionally, it improves the handling of completion
    window visibility and closing logic.

--- Sources/STTextViewAppKit/STTextView+Complete.swift
+++ Sources/STTextViewAppKit/STTextView+Complete.swift
@@ -9,6 +9,7 @@ extension STTextView {
     /// Supporting Autocomplete
     ///
     /// see NSStandardKeyBindingResponding
+    @MainActor
     open override func complete(_ sender: Any?) {
         if let completionWindowController, completionWindowController.isVisible {
             completionWindowController.close()
@@ -17,29 +18,29 @@ extension STTextView {
         }
     }
 
+    @MainActor
     open override func cancelOperation(_ sender: Any?) {
         self.complete(sender)
     }
 
     @MainActor
     private func performCompletion() {
+        dispatchPrecondition(condition: .onQueue(.main))
+
         guard let insertionPointLocation = textLayoutManager.insertionPointLocations.first,
-              let textCharacterSegmentRect = textLayoutManager.textSegmentFrame(at: insertionPointLocation, type: .standard)
+              let textCharacterSegmentRect = textLayoutManager.textSegmentFrame(at: insertionPointLocation, type: .standard),
+              let completionItems = delegateProxy.textView(self, completionItemsAtLocation: insertionPointLocation),
+              completionItems.isEmpty == false
         else {
-            self.completionWindowController?.close()
+            if let completionWindowController, completionWindowController.isVisible {
+                self.completionWindowController?.close()
+            }
             return
         }
 
-        // move left by arbitrary 14px
-        let characterSegmentFrame = textCharacterSegmentRect.moved(dx: -14, dy: textCharacterSegmentRect.height)
-
-        let completionItems = delegateProxy.textView(self, completionItemsAtLocation: insertionPointLocation) ?? []
-
-        dispatchPrecondition(condition: .onQueue(.main))
-
-        if completionItems.isEmpty {
-            self.completionWindowController?.close()
-        } else if let window = self.window {
+        if let window = self.window {
+            // move left by arbitrary 14px
+            let characterSegmentFrame = textCharacterSegmentRect.moved(dx: -14, dy: textCharacterSegmentRect.height)
             let completionWindowOrigin = window.convertPoint(toScreen: contentView.convert(characterSegmentFrame.origin, to: nil))
             completionWindowController?.showWindow(at: completionWindowOrigin, items: completionItems, parent: window)
             completionWindowController?.delegate = self
