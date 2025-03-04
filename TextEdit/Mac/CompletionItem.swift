701000544ef7d172feac0302f88fa7ce477d0a1a
Marcin Krzyzanowski <marcin.krzyzanowski@gmail.com>
2025-03-04 21:08:32 +0100

    Improve appearance of completion menu items with better styling and layout
    The changes enhance the visual presentation of completion menu items by adding proper padding,
    multicolor symbol rendering, font styling, and color adjustments based on the system color
    scheme.

--- TextEdit/Mac/CompletionItem.swift
+++ TextEdit/Mac/CompletionItem.swift
@@ -11,17 +11,37 @@ enum Completion {
         let insertText: String
 
         var view: NSView {
-            NSHostingView(rootView: VStack(alignment: .leading) {
-                HStack {
-                    Image(systemName: symbolName)
-                        .frame(width: 24)
-
-                    Text(label)
-
-                    Spacer()
-                }
-            })
+            NSHostingView(rootView: ItemView(label: label, symbolName: symbolName))
         }
     }
 
+}
+
+private struct ItemView: View {
+    @Environment(\.colorScheme) var colorScheme
+
+    let label: String
+    let symbolName: String
+
+    var body: some View {
+        VStack(alignment: .leading) {
+            HStack {
+                Image(systemName: symbolName)
+                    .symbolRenderingMode(.multicolor)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .foregroundStyle(Color.accentColor, Color(nsColor: .secondaryLabelColor))
+                    .frame(maxWidth: 12)
+                    .cornerRadius(2)
+
+                Text(label)
+                    .fontWeight(.regular)
+                    .foregroundStyle(Color(nsColor: .secondaryLabelColor))
+
+                Spacer()
+            }
+            .monospacedDigit()
+            .padding(.horizontal, 6)
+        }
+    }
 }
